ac24ac3b7cb1759c9f425eaca42c5a01761121f1
AndreSci <scitecever@gmail.com>
2023-03-20 17:20:27 +0300

    keep going to do small fix

--- misc/take_uid.py
+++ misc/take_uid.py
@@ -1,8 +1,10 @@
 class UserUid:
+    """ Класс обработки связанных с UID """
 
     @staticmethod
     def take(f_apacs_id: int, fid: int) -> int:
         """ Принимает FApacsID и FID """
+
         # Побитовый сдвиг для получения FUID
         uid = (fid << 32) | f_apacs_id
 
